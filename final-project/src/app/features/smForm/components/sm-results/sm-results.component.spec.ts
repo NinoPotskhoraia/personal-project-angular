c0548be2f1f646750d3d03d4d79916d79ad71e2c
NinoPotskhoraia <nino.potskhoraia17@gmail.com>
2022-09-17 22:58:32 +0400

    added responsive

--- final-project/src/app/features/smForm/components/sm-results/sm-results.component.spec.ts
+++ final-project/src/app/features/smForm/components/sm-results/sm-results.component.spec.ts
@@ -1,11 +1,9 @@
-import {
-  HttpClient,
-  HttpClientModule,
-  HttpHandler,
-} from '@angular/common/http';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SmResultsComponent } from './sm-results.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SmService } from '../../services/sm.service';
 
 describe('SmResultsComponent', () => {
   let component: SmResultsComponent;
@@ -14,7 +12,8 @@ describe('SmResultsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SmResultsComponent],
-      providers: [HttpClient, HttpHandler],
+      providers: [HttpClient, HttpHandler, SmService],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   });
 
